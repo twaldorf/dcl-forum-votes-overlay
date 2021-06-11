4961fd3ea0016f8c440fe441e27c1bebf48d5efa
twaldorf <trevor.automated@gmail.com>
2021-06-10 18:16:15 -0700

    add styles, page updating

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -1,4 +1,9 @@
+const containerId = 'pluginStats'
+
+const $c = (_) => document.createElement(_)
+
 const render = (data) => {
+    cleanup()
     const totalVp = Object.keys(data).reduce((prev, curr) => {
         return prev + data[curr].vp
     },0)
@@ -14,39 +19,80 @@ const render = (data) => {
     })
     const percentOfTotal1 = Math.trunc(total1/totalVp * 100)
     const percentOfTotal2 = Math.trunc(total2/totalVp * 100)
-    console.log(totalVp, total1, total2, percentOfTotal1, percentOfTotal2)
 
-    const stats = document.createElement('div')
-    stats.id = 'pluginStats'
-    const vpPercent1 = document.createElement('li').textContent = `Yes: ${percentOfTotal1}%`
-    const vpPercent2 = document.createElement('li').textContent = `No: ${percentOfTotal2}%`
-    stats.style.position = 'fixed'
-    stats.style.right = '1rem'
-    stats.style.bottom = '1rem'
-    stats.style.backgroundColor = '#ffffff'
-    stats.style.display = 'flex'
+    const stats = $c('div')
+    const title = $c('div')
+    stats.classList.add(containerId)
+    
+    title.textContent = 'CURRENT RESULTS'
+
+    // build progress bars
+    const progress1 = $c('div')
+    progress1.classList.add('progress-bar-color')
+    progress1.style.width = `${percentOfTotal1}%`
+    const progress2 = $c('div')
+    progress2.classList.add('progress-bar-color')
+    progress2.style.width = `${percentOfTotal2}%`
+    const progress1grey = $c('div')
+    progress1grey.classList.add('progress-bar-grey')
+    const progress2grey = $c('div')
+    progress2grey.classList.add('progress-bar-grey')
+
+    const progressBatch1 = $c('div')
+    progressBatch1.classList.add('progressBarGroup')
+    progressBatch1.append(progress1)
+    progressBatch1.append(progress1grey)
+
+    const progressBatch2 = $c('div')
+    progressBatch2.classList.add('progressBarGroup')
+    progressBatch2.append(progress2)
+    progressBatch2.append(progress2grey)
+
+    //build percents
+    const vpPercent1 = $c('li')
+    vpPercent1.textContent = `Yes: ${percentOfTotal1}%`
+    const vpPercent2 = $c('li')
+    vpPercent2.textContent = `No: ${percentOfTotal2}%`
+
+    //assemble final panel
+    vpPercent1.append(progressBatch1)
+    vpPercent2.append(progressBatch2)
+    stats.append(title)
+    stats.append(vpPercent1)
+    stats.append(vpPercent2)
+
     document.querySelector('body').append(stats)
-    document.getElementById('pluginStats').append(vpPercent1)
-    document.getElementById('pluginStats').append(vpPercent2)
 }
 
-const setup = () => {
-    const link = Array.from(document.querySelectorAll('a')).find(e => e.innerText == "Vote on this proposal on the Decentraland DAO")
+function setup () {
+    const link = Array.from(document.querySelectorAll('a')).find(e => e.innerText.includes("Vote on this proposal on the Decentraland DAO"))
     if (link) {
         const href = link.href
         const regex = /(?<=\?id=).*/g
         const id = href.match(regex)
         try {
             const votes = fetch(`https://governance.decentraland.org/api/proposals/${id}/votes`)
-                .then(response => response.json()).then(data => {render(data.data)})
+                .then(response => response.json()).then(data => {
+                    render(data.data)
+                })
         } catch (e) {
             console.error(e)
         }
+    } else {
+        cleanup()
     }
 }
 
-setup()
+function cleanup () {
+    try {
+        Array.from(document.getElementsByClassName(containerId)).map(e => {e.remove()})
+    } catch(e) {}
+}
 
-window.addEventListener('onhashchange', () => {
+window.onload = setup
+
+chrome.runtime.onMessage.addListener(() => {
+    console.log('updated');
+    cleanup()
     setup()
-})
+});
